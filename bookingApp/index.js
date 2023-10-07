22be48cbe9f619294f58de8842500d580cea90c5
Seemachal Goud <80570045+Seemachal-Goud-au19@users.noreply.github.com>
2023-10-07 15:38:25 +0530

    axios get in crudcrud

--- bookingApp/index.js
+++ bookingApp/index.js
@@ -16,3 +16,25 @@ form.addEventListener('submit', function (e) {
 
 
 });
+
+const showData =(res)=>{
+const data = res.data
+const userList = document.getElementById('userList');
+userList.innerHTML = '';
+
+data.forEach((user, index) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `${user.name} - ${user.email} - ${user.mobile}`
+                        
+    userList.appendChild(listItem);
+})
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+   axios.get('https://crudcrud.com/api/0088f909759e4857a0b41f8b73839265/appointment')
+   .then((res)=>showData(res))
+   .catch(err=>console.log(err))
+  });
+
+
+  
