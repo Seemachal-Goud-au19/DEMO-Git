c37774ad82e93a948d88f627818f114af5a9cde3
Seemachal Goud <80570045+Seemachal-Goud-au19@users.noreply.github.com>
2023-10-07 15:59:58 +0530

    update axios crudcrud

--- bookingApp/index.js
+++ bookingApp/index.js
@@ -25,15 +25,27 @@ axios.delete(`https://crudcrud.com/api/0088f909759e4857a0b41f8b73839265/appointm
 .catch(err=>console.log('delete',err))
 }
 
+const updateUser = (id)=>{
+    const updateData={
+        name:'lama',
+        email:'lama@gmail.com',
+        mobile:"987987w4987"
+    }
+    axios.delete(`https://crudcrud.com/api/0088f909759e4857a0b41f8b73839265/appointment/${id}`,updateData)
+    .then(res=>log("update",res))
+    .catch(err=>console.log('update',err))
+    }
+
 //show user
 const showData =(res)=>{
 const data = res.data
 const userList = document.getElementById('userList');
 userList.innerHTML = '';
-
+console.log("data",data);
 data.forEach((user, index) => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `${user.name} - ${user.email} - ${user.mobile}
+                          <button onclick="updateUser(${user._id})">Update</button>
                          <button onclick="deleteUser(${user._id})">Delete</button>`
                         
     userList.appendChild(listItem);
